cae3a9cc90c06eb60bda953f3765643db183115f
KasiaTorz <ktorz@wp.pl>
2018-03-09 12:50:46 +0100

    add click event on board

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  clicked(event){
+    console.log(event.target);
+  }
   render() {
     return (
         <div id="game">
             <div id="head">
               Welcome to Tic Tac Toe API game
             </div>
-            <div id="board">
+            <div id="board" onClick={(e)=>this.clicked(e)}>
                 <div class="div squears"></div>
                 <div class="div squears"></div>
                 <div class="div squears"></div>
