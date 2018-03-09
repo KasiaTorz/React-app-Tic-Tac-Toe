8553a45f825c391f3ccb2d9c1023f39754bacc8b
KasiaTorz <ktorz@wp.pl>
2018-03-09 15:41:07 +0100

    conect move with state

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     this.state={
       turn:'x',
         gameEnded:false,
+        winner:undefined,
         board:Array(9).fill(''),
         totalMoves:0
     }
@@ -26,13 +27,28 @@ class App extends Component {
             totalMoves:this.state.totalMoves++
     })
     }
+    var result = this.checkWinner();
+    if (result == 'x'){
+    this.setState({
+        gameEnded:true,
+        winner:'x'
+        });
+    else if (result == 'o'){
+      this.setState({
+          gameEnded:true,
+          winner:'x',
+      });
+        }
+    }
+
     checkWinner(){
 
     var moves=[[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],[0,1,2],[3,4,5],[6,7,8]]
     var board = this = this.state.board;
           for(let i=0;i<moves.length;i++){
       if (board[moves [i][0]] == board[moves [1][1]] && board[moves][1][1] == board[moves][i][2]])
-    }
+     return board[move [i] [0]];
+          }
 
     }
   }
