489db12d750b464b362a8dc364c8cdfa69b9dadf
KasiaTorz <ktorz@wp.pl>
2018-03-09 16:18:47 +0100

    change date state on gameState

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -9,19 +9,22 @@ class App extends Component {
             gameEnded: false,
             winner: undefined,
             board: Array(9).fill(''),
-            totalMoves: 0
         }
+        this.gameState={
+            board:Array(9).fill(''),
+            totalMoves:0
+        }
+
     }
 
     clicked(event) {
         if (this.state.board[event.target.datestate.square] == '') {
-            this.state.board[event.target.datestate.square] = this.state.turn;
+            this.gameState.board[event.target.datestate.square] = this.state.turn;
             event.target.innerText = this.state.turn;
             this.setState({
                 turn: this.state.turn == 'x' ? 'o' : 'x',
-                board: this.state.board,
-                totalMoves: this.state.totalMoves++
             })
+
         }
         var result = this.checkWinner();
         if (result == 'x') {
@@ -48,13 +51,13 @@ class App extends Component {
 
     checkWinner() {
         var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
-        var board = this.state.board;
+        var board = this.gameState.board;
         for (let i = 0; i < moves.length; i++) {
             if (board[moves [i][0]] == board[moves [i][1]] &&
                 board[moves][i][1] == board[moves][i][2]]
             return board[move [i] [0]];
         }
-        if(this.state.totalMoves == 9){
+        if(this.gameState.totalMoves == 9){
          return 'draw'
     }
 }
