8841f0feaf765a7968b9e3c5b16d8558f2db33c3
KasiaTorz <ktorz@wp.pl>
2018-03-10 17:54:33 +0100

    removes errors from app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -18,28 +18,29 @@ class App extends Component {
     }
 
     clicked(event) {
-        if (this.state.board[event.target.datestate.square] == '') {
+        if (this.state.board[event.target.datestate.square] === '') {
             this.gameState.board[event.target.datestate.square] = this.state.turn;
             event.target.innerText = this.state.turn;
             this.setState({
-                turn: this.state.turn == 'x' ? 'o' : 'x',
+                turn: this.state.turn === 'x' ? 'o' : 'x',
             })
 
         }
         var result = this.checkWinner();
-        if (result == 'x') {
+        if (result === 'x') {
             this.setState({
                 gameEnded: true,
                 winner: 'x',
                 winnerLine:"Match wan by x",
             });
-        } else if (result == 'o') {
+        }
+        else if (result ==='o') {
             this.setState({
                 gameEnded: true,
                 winner: 'o',
                 winnerLine:"Match won by o"
             });
-            else if (result == 'draw'){
+            if (result === 'draw'){
                 this.setState({
                     gameEnded:true,
                     winner:'draw',
@@ -53,11 +54,11 @@ class App extends Component {
         var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]]
         var board = this.gameState.board;
         for (let i = 0; i < moves.length; i++) {
-            if (board[moves [i][0]] == board[moves [i][1]] &&
-                board[moves][i][1] == board[moves][i][2]]
-            return board[move [i] [0]];
+            if(board[moves[i][0]] === board[moves [i][1]]&&
+                board[moves][i][1] === board[moves][i][2])
+            return board[moves[i][0]];
         }
-        if(this.gameState.totalMoves == 9){
+        if(this.gameState.totalMoves === 9){
          return 'draw'
     }
 }
