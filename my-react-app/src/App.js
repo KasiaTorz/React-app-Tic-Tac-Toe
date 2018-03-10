228a04939601e3c54543023790126a510d5660bd
KasiaTorz <ktorz@wp.pl>
2018-03-10 18:12:44 +0100

    remove errors

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import './App.css';
+import "./App.css";
+
+
 
 class App extends Component {
     constructor() {
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.css';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <App.js/>,
   document.getElementById('root')
 );
